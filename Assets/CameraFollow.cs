dbb4b63095ad1f5553cebb2b05236ba066680deb
NimaPeykarimeh <nimapeykerimeh87@gmail.com>
2025-07-28 14:15:09 +0300

    kodlara notlar eklendi

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    // Takip edilecek oyuncunun Transform'u (pozisyon, dönüþ, ölçek bilgisi)
     [SerializeField] Transform playerTransform;
-    [SerializeField] Vector3 followOffset;
-    
 
-    
+    // Kameranýn oyuncuya göre ne kadar uzakta duracaðýný belirler (x, y, z)
+    [SerializeField] Vector3 followOffset;
+
+    // Oyuncunun hareketinden sonra çalýþýr, kamera güncellenir
     void LateUpdate()
     {
+        // Kameranýn pozisyonu = oyuncunun pozisyonu + ofset (uzaklýk)
         transform.position = playerTransform.position + followOffset;
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,51 +2,63 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //[HideInInspector] public float speed;
+    // Oyuncunun fiziksel hareketlerini kontrol etmek için Rigidbody bileþeni
     [SerializeField] private Rigidbody rb;
+
+    // Yerdeyken hareket hýzý
     [SerializeField] private float speed = 5f;
 
-    [SerializeField] float jumpSpeed;
-    [SerializeField] bool isGrounded;
+    // Zýplama hýzý (yani yukarý doðru ne kadar güçlü zýplanacaðý)
+    [SerializeField] private float jumpSpeed = 5f;
+
+    // Oyuncu þu anda yerde mi? Bunu kontrol ederiz ki sadece yerdeyken zýplasýn
+    [SerializeField] private bool isGrounded;
 
     private void Update()
     {
-        float horizontalMove = Input.GetAxisRaw("Horizontal");
-        float verticalMove = Input.GetAxisRaw("Vertical");
-        Vector3 move = new Vector3(horizontalMove, 0f, verticalMove).normalized * speed;
+        // Yön tuþlarýna veya WASD'ye basýldýðýnda alýnan giriþ
+        float horizontalMove = Input.GetAxisRaw("Horizontal"); // A-D veya Sol-Sað
+        float verticalMove = Input.GetAxisRaw("Vertical");     // W-S veya Ýleri-Geri
 
+        // Hareket yönü (x ve z ekseni) ve hýz çarpýmý
+        Vector3 move = new Vector3(horizontalMove, 0f, verticalMove).normalized * speed;
 
         float yVelocity;
 
+        // Space tuþuna basýlýrsa ve oyuncu yerdeyse zýplama baþlar
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            yVelocity = jumpSpeed;
-            isGrounded = false;
+            yVelocity = jumpSpeed; // yukarý doðru hýz ver
+            isGrounded = false;    // artýk havadayýz
         }
         else
         {
-            yVelocity = rb.linearVelocity.y;
+            yVelocity = rb.linearVelocity.y; // y ekseni hýzýný koru (örneðin düþerken)
         }
 
-        Vector3 velocity = new Vector3(move.x,yVelocity,move.z) ;
+        // Yeni hýz vektörü (x ve z ekseni için hareket + y ekseni için zýplama/düþme)
+        Vector3 velocity = new Vector3(move.x, yVelocity, move.z);
 
+        // Rigidbody'nin hýzýný ayarla (karakteri hareket ettir)
         rb.linearVelocity = velocity;
     }
 
+    // Bir yere çarpýldýðýnda çaðrýlýr
     private void OnCollisionEnter(Collision collision)
     {
-        //collision.gameObject.tag == "Ground"
+        // Eðer yere çarptýysak (Ground etiketi varsa), yerdeyiz
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
     }
+
+    // Yerden ayrýldýðýmýzda (havaya zýpladýðýmýzda) çaðrýlýr
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
-
         }
     }
 }
